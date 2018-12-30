6daf649d52d9101d0661a73d67f5a6000569fd1f
zhonglunsheng <873946453@qq.com>
2018-12-30 15:27:15 +0800

    fix bug 集成mybatis-plus出现错误

--- src/main/java/com/exam/online/ExamOnlineApplication.java
+++ src/main/java/com/exam/online/ExamOnlineApplication.java
@@ -1,9 +1,11 @@
 package com.exam.online;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.exam.online.mapper")
 public class ExamOnlineApplication {
 
     public static void main(String[] args) {
