2af688b98700e1ebef14549d951a07438c0ad439
zhonglunsheng <873946453@qq.com>
2018-12-30 15:00:33 +0800

    Initial commit

---
+++ src/main/java/com/exam/online/ExamOnlineApplication.java
@@ -0,0 +1,14 @@
+package com.exam.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamOnlineApplication.class, args);
+    }
+
+}
+
