6dc1e4937d3caebdb0cb9e4f39c606eb255f1eb9
zhonglunsheng <873946453@qq.com>
2019-01-19 19:55:03 +0800

    添加权限判断

--- src/main/java/com/exam/online/controller/UserController.java
+++ src/main/java/com/exam/online/controller/UserController.java
@@ -1,13 +1,23 @@
 package com.exam.online.controller;
 
 
+import cn.hutool.crypto.SecureUtil;
+import com.exam.online.common.Result;
 import com.exam.online.entity.User;
+import com.exam.online.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
- * 存储管理员、教师信息 前端控制器
+ * 管理员、教师相关操作
  * </p>
  *
  * @author zhonglunsheng
@@ -18,16 +28,42 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/index")
     public String index(){
         return "hello world~";
     }
 
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String login(String email){
-        log.info(email);
-        return "hello world~";
+    /**
+     * 管理员登陆
+     * @param user
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "/login")
+    public Result login(User user, HttpServletRequest request){
+        //账号和密码是否存在
+        String email = user.getEmail();
+        String password = user.getPassword();
+
+        if (Strings.isBlank(email) || Strings.isBlank(password)){
+            return Result.error("账号或密码错误");
+        }
+
+        Map<String, Object> columnMap = new HashMap<>();
+        columnMap.put("email", email);
+        columnMap.put("password", password);
+        List<User> list = (List<User>) userService.listByMap(columnMap);
+        if (list.size() != 0){
+            request.getSession().setAttribute("admin", user);
+            return Result.success();
+        }else{
+            return Result.error("用户名不存在");
+        }
     }
+
 
 }
 
